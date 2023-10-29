b745103f1f65cef65592fd081db4e30a46f0ce84
Venus00 <oussama@nextronic.io>
2023-10-29 12:07:18 +0100

    fix

--- src/renderer/store/Dispatch.tsx
+++ src/renderer/store/Dispatch.tsx
@@ -1,36 +1,57 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { updateDataState } from "./slice/data";
-
 
 export default function Dispatch() {
 
     const dispatch = useDispatch()
-
+	const clientRef = useRef<WebSocket | null>(null);
+	const [waitingToReconnect, setWaitingToReconnect] = useState<boolean | null>(null);
     useEffect(() => {
 
-		function connect() {
-			var ws = new WebSocket('ws://localhost:1880/data');
-		 
-			ws.onmessage = function(event) {
-			dispatch(updateDataState(JSON.parse(event.data)))
-			};
-		  
-			ws.onclose = function(e) {
-			  console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
-			  setTimeout(function() {
-				connect();
-			  }, 1000);
-			};
-			ws.onerror = function(err) {
-				setTimeout(function() {
-					connect();
-				  }, 1000);
-			};
+		if (waitingToReconnect) {
+			return;
 		}
-		connect()
+		if(!clientRef.current) {
+			const client = new WebSocket('ws://localhost:8080');
+			clientRef.current = client;
+			client.onclose = function() {
+				if(clientRef.current)
+				{
+					console.log('connection closed by the servr');
+				}
+				else
+				{
+					console.log('useEffect Distroy');
+					return;
+				}
+
+				if(waitingToReconnect) return;
+				console.log('ws is closed');
+				setWaitingToReconnect(true);
+
+				setTimeout(()=> setWaitingToReconnect(null),1000);
+			};
+			client.onopen = () => {
+				console.log('ws opened');
+			  };
+			client.onerror = (e) => console.error(e);
+			if (waitingToReconnect) {
+				return;
+			};
+			client.onmessage = function(event) {
+				dispatch(updateDataState(JSON.parse(event.data)))
+			};
+			 	
 		return () => {
+			console.log('distroy');
+			clientRef.current = null;
+			client.close();
 		};
-	  }, []);
+
+
+		}
+	
+	  }, [waitingToReconnect]);
 	  return null;
 }
